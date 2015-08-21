979941c76c7e300dc5a45db9ca1637a404574fac
Owen Synge <osynge@suse.com>
2015-08-21 16:03:23 +0200

    install: 'ceph-radosgw' removed from defaults.
    Since the suse rgw code installs components as needed we
    do not need to by default install 'ceph-radosgw'
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_deploy/util/constants.py
+++ ceph_deploy/util/constants.py
@@ -26,5 +26,5 @@ default_components = namedtuple('DefaultComponents', ['rpm', 'deb'])
 
 # the difference here is because RPMs currently name the radosgw differently than DEBs.
 # TODO: This needs to get unified once the packaging naming gets consistent
-default_components.rpm = tuple(_base_components + ['ceph-radosgw'])
+default_components.rpm = tuple(_base_components)
 default_components.deb = tuple(_base_components + ['radosgw'])
